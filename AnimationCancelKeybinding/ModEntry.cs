049d2390984f30d70d7b63b8c7497caa70218852
focustense <focustense@protonmail.com>
2024-07-30 14:04:48 -0400

    Improve controller support + general tidying.
    - Updates target to .NET 6 (current LTS, expected by SMAPI)
    - Adds missing namespace declaration
    - Fixes root namespace and adds some missing project attributes
    - Adds l10n for GMCM settings
    - Adds input suppression when using keys with other potential functions (e.g. ControllerY)
    - Adds config/GMCM toggle for input suppression behavior, either suppress the key always (completely disabling its normal function) or only when performing an animation cancel.
    - Version bump.

--- AnimationCancelKeybinding/ModEntry.cs
+++ AnimationCancelKeybinding/ModEntry.cs
@@ -1,57 +1,94 @@
-﻿
-using GenericModConfigMenu;
+﻿using GenericModConfigMenu;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
 
+namespace AnimationCancelKeybinding;
 
 /// <summary>The mod entry point.</summary>
 internal sealed class ModEntry : Mod
 {
     private ModConfig config;
+
     /// <summary>The mod entry point, called after the mod is first loaded.</summary>
     /// <param name="helper">Provides simplified APIs for writing mods.</param>
     public override void Entry(IModHelper helper)
     {
+        I18n.Init(Helper.Translation);
         config = helper.ReadConfig<ModConfig>();
-        helper.Events.Input.ButtonsChanged += cancelOnChange;
-
-        helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+        helper.Events.Input.ButtonsChanged += Input_ButtonsChanged;
+        helper.Events.GameLoop.GameLaunched += GameLoop_GameLaunched;
     }
 
-    private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+    private void GameLoop_GameLaunched(object sender, GameLaunchedEventArgs e)
     {
-        var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+        var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
         if (configMenu is null)
             return;
-
-        config = Helper.ReadConfig<ModConfig>();
         
         // register mod
         configMenu.Register(
-            mod: this.ModManifest,
-            reset: () => this.config = new ModConfig(),
-            save: () => this.Helper.WriteConfig(this.config)
+            mod: ModManifest,
+            reset: () => config = new ModConfig(),
+            save: () => Helper.WriteConfig(config)
         );
         
-        configMenu.AddKeybindList( mod: this.ModManifest,
-            name: () => "Animation-Cancel key",
-            tooltip: () => "Which key to press to cancel the current animation",
-            getValue: () => this.config.CancelKey,
-            setValue: value => this.config.CancelKey= value);
+        configMenu.AddKeybindList(
+            ModManifest,
+            name: () => I18n.Gmcm_Keybind_Name(),
+            tooltip: () => I18n.Gmcm_Keybind_Tooltip(),
+            getValue: () => config.CancelKey,
+            setValue: value => config.CancelKey = value);
+        configMenu.AddTextOption(
+            ModManifest,
+            name: () => I18n.Gmcm_Suppression_Name(),
+            tooltip: () => I18n.Gmcm_Suppression_Tooltip(I18n.Gmcm_Keybind_Name()),
+            getValue: () => config.Suppression.ToString(),
+            setValue: value => config.Suppression = Enum.Parse<KeySuppression>(value),
+            allowedValues: Enum.GetValues<KeySuppression>().Select(v => v.ToString()).ToArray(),
+            formatAllowedValue: value => Helper.Translation.Get($"Gmcm.Suppression.Option.{value}"));
     }
 
-    private void cancelOnChange(object sender, ButtonsChangedEventArgs e)
+    private void Input_ButtonsChanged(object sender, ButtonsChangedEventArgs e)
     {
-        if (!config.CancelKey.JustPressed()) return;
-        var f = Game1.player;
-        if (!f.UsingTool) return;
-        f.completelyStopAnimatingOrDoingAction();
+        if (Game1.freezeControls)
+        {
+            return;
+        }
+        var keybind = config.CancelKey.GetKeybindCurrentlyDown();
+        if (keybind is null)
+        {
+            return;
+        }
+        var player = Game1.player;
+        if (!player.UsingTool)
+        {
+            if (config.Suppression == KeySuppression.Always)
+            {
+                SuppressKeybind(keybind);
+            }
+            return;
+        }
+        player.forceCanMove();
+        player.completelyStopAnimatingOrDoingAction();
+        player.UsingTool = false;
+        SuppressKeybind(keybind);
+    }
+
+    private void SuppressKeybind(Keybind keybind)
+    {
+        foreach (var button in keybind.Buttons)
+        {
+            Helper.Input.Suppress(button);
+        }
     }
 }
+
+enum KeySuppression { OnCancel, Always }
 
 class ModConfig
 {
     public KeybindList CancelKey { get; set; } = KeybindList.Parse("Space");
+    public KeySuppression Suppression { get; set; } = KeySuppression.OnCancel;
 }
